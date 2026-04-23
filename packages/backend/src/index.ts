fe4b0312e84dedcfc50bfaee08c4bb5bbdd56576
Your Name <your.email@example.com>
2026-04-23 10:47:29 +0100

    Revert search module, keep frontend build fix

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -50,7 +50,7 @@ backend.add(import('@backstage/plugin-search-backend'));
 
 // search engine
 // See https://backstage.io/docs/features/search/search-engines
-backend.add(import('@backstage/plugin-search-backend-module-lunr'));
+backend.add(import('@backstage/plugin-search-backend-module-pg'));
 
 // search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
